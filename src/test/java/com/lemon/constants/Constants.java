0dc6f890078de2be727725f98076d4ea4aba68fb
bizi38585359 <182240846@qq.com>
2020-07-06 00:50:35 +0800

    去掉空格

--- src/test/java/com/lemon/constants/Constants.java
+++ src/test/java/com/lemon/constants/Constants.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Constants {
     //数据驱动excel路径
     //public static final String EXCEL_PATH = Constants.class.getClassLoader().getResource("./cases_v3.xlsx").getPath();
-    public static final String EXCEL_PATH = System.getProperty("user.dir ") + "/src/test/resources/cases_v3.xlsx";
+    public static final String EXCEL_PATH = System.getProperty("user.dir") + "/src/test/resources/cases_v3.xlsx";
     //默认请求头
     public static final Map<String,String> HEADERS = new HashMap<>();
     //excel 响应回写列
